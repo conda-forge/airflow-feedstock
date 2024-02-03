9cfa694b2a5a12a00a927048ea7db6a78aa5360c
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-02-03 20:43:21 +0100

    Manually download required dev files

--- recipe/install_airflow.sh
+++ recipe/install_airflow.sh
@@ -2,4 +2,12 @@
 
 set -xe
 
+# dev/hatch_build.py was purposefully removed from the release but we need it
+mkdir -p dev
+for file in hatch_build.py airflow_pre_installed_providers.txt
+do
+  curl -SL "https://raw.githubusercontent.com/apache/airflow/${PKG_VERSION}/dev/${file}" \
+      > "dev/${file}"
+done
+
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
